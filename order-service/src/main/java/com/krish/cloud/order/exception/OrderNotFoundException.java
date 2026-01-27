5deeaff0530a9a76fd761a1745400a0b0acaf808
krish <krish.tomar_cs23@gla.ac.in>
2026-01-27 22:22:16 +0530

    Remove order-service files from customer-service tracking

--- order-service/src/main/java/com/krish/cloud/order/exception/OrderNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.krish.cloud.order.exception;
-
-public class OrderNotFoundException extends RuntimeException {
-
-    public OrderNotFoundException(Long id) {
-        super("Order not found with id: " + id);
-    }
-}
--- order-service/src/main/java/com/krish/cloud/order/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.krish.cloud.order.repository;
-
-import com.krish.cloud.order.entity.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
