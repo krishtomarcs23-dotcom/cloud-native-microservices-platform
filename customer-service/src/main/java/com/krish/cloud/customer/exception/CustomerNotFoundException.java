52d8aafa78ff8cc0ddddd16e87d52f16f8244636
krish <krish.tomar_cs23@gla.ac.in>
2026-01-27 22:22:02 +0530

    Remove customer-service files from order-service tracking

--- customer-service/src/main/java/com/krish/cloud/customer/exception/CustomerNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.krish.cloud.customer.exception;
-
-public class CustomerNotFoundException extends RuntimeException {
-    public CustomerNotFoundException(String message) {
-        super(message);
-    }
-}
